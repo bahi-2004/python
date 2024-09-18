3a69aed48fd54649f4a327c150d08b16d6631c6e
bahi-2004 <bahithoubi@gmail.com>
2024-09-18 12:33:31 +0530

    Create python18.py

---
+++ python18.py
@@ -0,0 +1,5 @@
+string = "Hello, World!"
+print("Length of the string:", len(string))
+print("Uppercase:", string.upper())
+print("Lowercase:", string.lower())
+print("Reversed string:", string[::-1])
